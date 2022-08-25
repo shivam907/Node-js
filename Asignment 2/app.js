1d38ad8c65cf42725ffd4d9857b061da2e0870c4
shivam907 <Shivamkaushal907@gmail.com>
2022-08-25 23:51:24 +0530

    An Update

---
+++ Asignment 2/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>users</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello</h1>");
+});
+app.listen(3000);
---
+++ Express/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  // console.log("This always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  // console.log("In the Another middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body["title"]);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  // console.log("In the Another middleware");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
