4666ec026c4a1e6ec3466814232b1752d1f641b2
Shivam907 <Shivamkaushal907@gmail.com>
2022-08-29 23:13:15 +0530

    An Update

--- Dynamic Express/app.js
+++ Dynamic Express/app.js
@@ -1,21 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const expressHbs = require("express-handlebars");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use('/admin', adminRoutes);
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { title: "Page not found sorry" });
 });
 
 app.listen(3000);
--- Dynamic Express/routes/shop.js
+++ Dynamic Express/routes/shop.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  console.log(adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
