46540899e547b2fae3cb61feff4b15ceac29772b
shivam907 <shivamkaushal907@gmail.com>
2022-10-07 04:11:34 +0530

    An Update

--- Task Manager/routes/getTask.js
+++ Task Manager/routes/getTask.js
@@ -2,4 +2,5 @@ const express = require("express");
 const controller = require("../controllers/getTask");
 
 const routers = express.Router();
+
 module.exports = routers.post("/tasks", controller);
